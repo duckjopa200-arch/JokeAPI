645c4f7e0b9a3fb9bf7d0d36c8cd4a21ba2c1eeb
Damian Adamson <damian.adamson@tptlive.ee>
2026-02-12 11:43:09 +0200

    777vor1597@ndit:)

---
+++ script.js
@@ -0,0 +1,16 @@
+/* https://www.youtube.com/watch?v=zUcc4vW-jsI*/
+
+const button = document.getElementById("getJoke");
+const jokeText = document.getElementById("joke"); 
+
+
+async function getData() {
+  const response = await fetch('https://official-joke-api.appspot.com/random_joke');
+  const data = await response.json();
+  console.log(data)
+
+  jokeText.textContent = data.setup + " — " + data.punchline;
+}
+
+
+button.addEventListener("click", getData);
